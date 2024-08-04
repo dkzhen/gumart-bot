b999fabb597a96967ff12aedc14fa8a6491f5a5f
dkzhen <dani314123@gmail.com>
2024-08-04 20:29:28 +0700

    initial commit

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const cron = require("node-cron");
 const express = require("express");
 
 const { configDotenv } = require("dotenv");
-const { validateToken } = require("./func/CheckValidToken");
 const { startBoost } = require("./func/startBoost");
 configDotenv();
 // Schedule the task to run every hour on the hour
-
-// cron.schedule("0 0 * * *", DailyRewards);
+startBoost();
+cron.schedule("0 0 * * *", startBoost);
 
 // Start the server
 const port = process.env.PORT || 104;
@@ -18,6 +17,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, async () => {
-  startBoost();
   console.log(`Server is running on port ${port}`);
 });
